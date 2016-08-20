ea1820fc6ef0def82a2e4e6123bd67fe92f50eff
Satyam <satya12100@hotmail.com>
2016-08-20 18:44:02 +0530

    home page

--- application/controllers/content.php
+++ application/controllers/content.php
@@ -2,7 +2,7 @@
 
 (defined('BASEPATH')) OR exit('No direct script access allowed');
 
-require './phpMailer/PHPMailerAutoload.php';
+//require './phpMailer/PHPMailerAutoload.php';
 
 class Content extends CI_Controller {
 
